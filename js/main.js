ef7587f8768f5413458ada8cd19c99837b0a9e94
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-04-08 01:31:59 -0300

    iniciando o modo escuro/claro

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let btt = document.querySelector(".botao").addEventListener("click", myFunction);
+
+let main = document.querySelector(".main").addEventListener(myFunction);
+
+
+
+
+function myFunction () {
+    
+    let fundo = document.body.style.background = "white";
+    
+    main = document.body.style.background = "white";
+};
+
+
+
+
+
