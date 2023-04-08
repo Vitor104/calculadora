cffa18772f09c173ad5ad5e705906b0b665d4ef8
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-04-08 19:29:49 -0300

    finalizei o modo noturno!
    conseguir criar um sistema de modo noturno, agora falta colocar para funcionar as teclas.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
-let btt = document.querySelector(".botao").addEventListener("click", myFunction);
 
-let main = document.querySelector(".main").addEventListener(myFunction);
+
+
 
 
 
 
 function myFunction () {
     
-    let fundo = document.body.style.background = "white";
+    let btt = document.querySelector(".body").classList.toggle("teste");
+
+    document.querySelector(".numeros").classList.toggle("change");
+
+    document.querySelector("#um").classList.toggle("change");
+    document.querySelector("#dois").classList.toggle("change");
+    document.querySelector("#tres").classList.toggle("change");
+    document.querySelector("#quatro").classList.toggle("change");
+    document.querySelector("#cinco").classList.toggle("change");
+    document.querySelector("#seis").classList.toggle("change");
+    document.querySelector("#sete").classList.toggle("change");
+    document.querySelector("#oito").classList.toggle("change");
+    document.querySelector("#nove").classList.toggle("change");
+    document.querySelector("#dez").classList.toggle("change");
+    document.querySelector("#mais").classList.toggle("change");
+    document.querySelector("#menos").classList.toggle("change");
+    document.querySelector("#vezes").classList.toggle("change");
+    document.querySelector("#divisao").classList.toggle("change");
+
+    document.querySelector(".resultado").classList.toggle("re");
+
+    document.querySelector(".botao").classList.toggle("btt");
     
-    main = document.body.style.background = "white";
+    
 };
 
 
 
 
 
+//let btt = document.querySelector(".botao").addEventListener("click", myFunction);
+
+//let fundo = document.body.style.background = "white";
